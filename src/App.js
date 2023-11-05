2a353daad9cf6998efaf193eb42b9c8a43630fb0
HELSING67666 <HELSING67666@gmail.com>
2023-11-05 11:28:54 -0500

    Inicio proyecto todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { TodoCounter } from './TodoCounter'
+import { TodoSearch } from './TodoSearch'
+import { TodoList } from './TodoList'
+import { TodoItem } from './TodoItem'
+import { CreateTodoButton } from './CreateTodoButton'
 
-function App() {
+const defaultTodos = [
+  { text: 'Prueba 1', completed: true },
+  { text: 'Prueba 2', completed: false },
+  { text: 'Prueba 3', completed: false },
+  { text: 'Prueba 4', completed: false },
+  { text: 'Prueba 5', completed: false }
+]
+
+function App () {
+  // eslint-disable-next-line no-unused-vars
+  const [todos, setTodos] = React.useState(defaultTodos)
+  const [searchValue, setSearchValue] = React.useState('')
+
+  const completedTodos = todos.filter(todo => todo.completed).length
+  const totalTodos = todos.length
+
+  const searchedTodos = todos.filter(
+    (todo) => {
+      const todoText = todo.text.toLowerCase()
+      const searchTodo = searchValue.toLowerCase()
+      return todoText.includes(searchTodo)
+    })
+
+  const completeTodos = (text) => {
+    const newTodos = [...todos]
+    const todoIndex = newTodos.findIndex(
+      todo => todo.text === text
+    )
+    newTodos[todoIndex].completed = true
+    setTodos(newTodos)
+  }
+
+  const deleteTodos = (text) => {
+    const newTodos = [...todos]
+    const todoIndex = newTodos.findIndex(
+      todo => todo.text === text
+    )
+    newTodos.splice(todoIndex, 1)
+    setTodos(newTodos)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    < >
+     <TodoCounter completed = {completedTodos} total={totalTodos} />
+     <TodoSearch
+      searchValue={searchValue}
+      setSearchValue={setSearchValue} />
+
+     <TodoList>
+        {searchedTodos.map(todo => (
+          <TodoItem key={todo.text}
+                    text={todo.text}
+                    completed={todo.completed}
+                    onComplete={() => completeTodos(todo.text)}
+                    onDelete={() => deleteTodos(todo.text)}
+                    />
+        ))}
+     </TodoList>
+
+     <CreateTodoButton/>
+    </>
+  )
 }
 
-export default App;
+export default App
